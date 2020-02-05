1433028b58f9e6cb2ffbd664b079761b59680d8a
nKapodistrias <nikolaos.kapodistrias@elumbus-reisen.de>
2020-02-05 15:03:21 +0200

    Introduces our new component, & fetches data from Marvel API

---
+++ src/components/MarvelExplorer.vue
@@ -0,0 +1,35 @@
+<template>
+  <p>An infinite list of superb comics &#129322;</p>
+</template>
+<script>
+/* eslint-disable */
+export default {
+  name: "MarvelExplorer",
+  data() {
+    return {
+      url: "https://gateway.marvel.com/v1/public/comics"
+    };
+  },
+  mounted: function() {
+    this.fetchComics();
+  },
+  methods: {
+    fetchComics: () => {
+      fetch(
+        "https://gateway.marvel.com/v1/public/comics?apikey=" +
+          process.env.VUE_APP_API_KEY,
+        {
+          method: "GET"
+        }
+      )
+        .then(data => {
+          console.log("Success:", data);
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
+    }
+  }
+};
+/* eslint-enable */
+</script>
