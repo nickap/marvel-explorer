bb67e48c1004cdd2af2c0fd44a9fa2b1a09e4fd6
nKapodistrias <nikolaos.kapodistrias@elumbus-reisen.de>
2020-02-05 17:41:41 +0200

    Showcases the first 20 images of commics

--- src/components/MarvelExplorer.vue
+++ src/components/MarvelExplorer.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>An infinite list of superb comics &#129322;</p>
+  <div class="container">
+    <p>An infinite list of superb comics &#129322;</p>
+    <div v-show="sorted" class="row">
+      <div class="col-6 col-md-3" v-for="(item, index) in data" :key="index">
+        <img class="img-fluid" :src="item.image"/>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 /* eslint-disable */
@@ -7,23 +14,36 @@ export default {
   name: "MarvelExplorer",
   data() {
     return {
-      url: "https://gateway.marvel.com/v1/public/comics"
+      url: "https://gateway.marvel.com/v1/public/comics?apikey=",
+      data: []
     };
   },
-  mounted: function() {
+  created: function() {
     this.fetchComics();
   },
+  computed: {
+    sorted: function() {
+      return this.data.length > 0;
+    }
+  },
   methods: {
-    fetchComics: () => {
-      fetch(
-        "https://gateway.marvel.com/v1/public/comics?apikey=" +
-          process.env.VUE_APP_API_KEY,
-        {
-          method: "GET"
-        }
-      )
-        .then(data => {
-          console.log("Success:", data);
+    fetchComics: function() {
+      fetch(this.url + process.env.VUE_APP_API_KEY, {
+        method: "GET"
+      })
+        .then(response => {
+          return response.json();
+        })
+        .then(responseJson => {
+          responseJson.data.results.forEach(item => {
+            this.data.push({
+              image: item.thumbnail.path + "." + item.thumbnail.extension,
+              title: item.title,
+              issueNumber: item.issueNumber,
+              price: item.prices.price
+            });
+          });
+          console.log(this.data);
         })
         .catch(error => {
           console.error("Error:", error);
