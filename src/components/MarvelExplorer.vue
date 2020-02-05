620c343a449ce2102c73085c9c378b5f50abb75d
nKapodistrias <nikolaos.kapodistrias@elumbus-reisen.de>
2020-02-05 20:59:53 +0200

    Introduces Infinite-list-feature  using Intersection Observer API

--- src/components/MarvelExplorer.vue
+++ src/components/MarvelExplorer.vue
@@ -3,8 +3,10 @@
     <p>An infinite list of superb comics &#129322;</p>
     <div v-show="sorted" class="row">
       <div class="col-6 col-md-3" v-for="(item, index) in data" :key="index">
-        <img class="img-fluid" :src="item.image"/>
+        <img class="img-fluid" :src="item.image" />
       </div>
+      <div ref="infiniteScrollTrigger" id="scroll-trigger"></div>
+      <div class="loader" v-if="showLoader"></div>
     </div>
   </div>
 </template>
@@ -15,16 +17,26 @@ export default {
   data() {
     return {
       url: "https://gateway.marvel.com/v1/public/comics?apikey=",
-      data: []
+      data: [],
+      // currentPage: 1,
+      // totalPages: 4,
+      showLoader: false
     };
   },
   created: function() {
     this.fetchComics();
   },
+  mounted() {
+    this.scrollTrigger();
+    this.showLoader = false;
+  },
   computed: {
     sorted: function() {
       return this.data.length > 0;
-    }
+    },
+    // pageCount() {
+    //   return this.totalPages - this.currentPage;
+    // }
   },
   methods: {
     fetchComics: function() {
@@ -48,8 +60,41 @@ export default {
         .catch(error => {
           console.error("Error:", error);
         });
+    },
+
+    scrollTrigger() {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.intersectionRatio > 0) {
+            this.showLoader = true;
+            this.fetchComics();
+            this.currentPage += 1;
+          }
+        });
+      });
+      observer.observe(this.$refs.infiniteScrollTrigger);
     }
   }
 };
 /* eslint-enable */
 </script>
+<style lang="css" scoped>
+.loader {
+  margin: auto;
+  margin-top: 50px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 5px solid rgba(125, 125, 125, 0.7);
+  border-top: 5px solid #eee;
+  animation: animate 1s infinite linear;
+}
+@keyframes animate {
+  0% {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+  100% {
+    transform: translate(-50%, -50%) rotate(360deg);
+  }
+}
+</style>
